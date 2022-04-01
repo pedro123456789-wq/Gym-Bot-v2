260f0f71cbda6d651145b037982ed346e2a0b784
Pedro Lourenco <pl156176@gmail.com>
2022-04-01 23:51:45 +0100

    Add alerts and re-direct to email confirmation page once account is created successfully

--- react-front-end/src/Components/SignUpPage/SignUpPage.tsx
+++ react-front-end/src/Components/SignUpPage/SignUpPage.tsx
@@ -1,22 +1,58 @@
 import Navbar from "../NavBar/Navbar";
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import RequestHandler from "../RequestHandler/RequestHandler";
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
+import Alert from "../Alert/Alert";
 
 
 function SignUpPage() {
+    // store user inputs
     const [userName, setUsername] = useState<string>();
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
     const [passwordConfirmation, setPasswordConfirmation] = useState<string>();
 
+    // handle loading icon and alerts
     const [isLoading, toggleLoad] = useState<boolean>(false);
+    const [showAlert, toggleAlert] = useState<boolean>(false);
+    const [alertMessage, setMessage] = useState<string>('');
+    const [isSuccess, toggleSuccess] = useState<boolean>(false);
+
+    // react navigation
+    const navigate = useNavigate();
+
 
 
     function createAccount(){
+        // check if password confirmation  matches password 
+        if (passwordConfirmation !== password){
+            toggleAlert(true);
+            setMessage('The two passwords do not match');
+            toggleSuccess(false);
+
+            // hide alert in 1 seconds
+            setTimeout(() => toggleAlert(false), 1000);
+            return;
+        }
+
+        toggleLoad(true);
+
         RequestHandler.Post('sign-up', {'username' : userName, 'password' : password, 'email' : email}).then(
             (response) => {
-                alert(response.message)
+                toggleLoad(false);
+                toggleAlert(true);
+                setMessage(response.message);
+                toggleSuccess(response.success);
+
+
+                //hide alert after 1 second
+                setTimeout(() => {
+                    toggleAlert(false)
+                    
+                    // redirect user to email confirmation page once account is created
+                    response.success && navigate('/confirm-email');
+                }, 1000);
             }
         )
     }
@@ -36,7 +72,11 @@ function SignUpPage() {
                 {isLoading &&
                     <LoadingIndicator />
                 }
-                
+
+                {showAlert && 
+                    <Alert message = {alertMessage} isSuccess = {isSuccess} /> 
+                }
+
                 <form>
                     <div className = 'form-group'>
                         <label htmlFor = 'email'>Email</label>
