2cc38de7d485dd847d288df84d658bd637f1a8e0
Pedro Lourenco <pl156176@gmail.com>
2022-05-04 21:39:30 +0100

    format file

--- flask_server/validationSchemes.py
+++ flask_server/validationSchemes.py
@@ -1,8 +1,7 @@
-'''JSON validation schemes for API endpoints'''
+'''JSON validation schemes for API endpoints and decorators'''
 
 
 '''/api/sign-up'''
-
 signUpSchema = {
     'type' : 'object', 
     'properties' : {
@@ -19,7 +18,8 @@ signUpSchema = {
         'email' : {
                     'type' : 'string', 
                     'minLength' : 1, 
-                    'maxLength' : 40
+                    'maxLength' : 40, 
+                    'pattern' : '[a-zA-Z0-9]*@[a-zA-Z0-9]*\.*[a-zA-Z0-9]*' #regex pattern for email validation
         }
     }, 
     'required' : ['username', 'email', 'password']
@@ -27,6 +27,8 @@ signUpSchema = {
 
 
 
+
+'''api/confirm-email'''
 confirmEmailSchema = {
     'type' : 'object', 
     'properties' : {
@@ -41,4 +43,172 @@ confirmEmailSchema = {
         }
     }, 
     'required' : ['username', 'confirmationCode']
+}
+
+
+
+
+'''api/log-in'''
+loginSchema = {
+    'type' : 'object', 
+    'properties' : {
+        'username' : {
+            'type' : 'string', 
+            'minLength' : 1, 
+            'maxLength' : 40
+        }, 
+        'password' : {
+            'type' : 'string', 
+            'minLength' : 1, 
+            'maxLength' : 40
+        }
+    }, 
+    'required' : ['username', 'password']
+}
+
+
+
+
+'''api/profile'''
+profileSchema = {
+    'type' : 'object', 
+    'properties' : {
+        'height' : {
+            'type' : 'number', 
+        },
+
+        'weight' : {
+            'type' : 'number'
+        },
+
+        'vo2Max' : {
+            'type' : 'number'
+        },
+
+        'age' : {
+            'type' : 'number'
+        }, 
+
+        'gender' : {
+            'type' : 'number'
+        }, 
+
+        'caloriesEatenTarget' : {
+            'type' : 'number'
+        }, 
+
+        'caloriesBurnedTarget' : {
+            'type' : 'number'
+        }, 
+
+        'minutesTrainedTarget' : {
+            'type' : 'number'
+        }, 
+
+        'distanceRanTarget' : {
+            'type' : 'number'
+        }
+    }, 
+    'required' : ['height', 'weight', 'vo2Max', 'age', 'gender', 'caloriesEatenTarget', 
+                  'caloriesBurnedTarget', 'minutesTrainedTarget', 'distanceRanTarget']
+}
+
+
+
+
+profileBounds = {
+            'height' : {
+                'max' : 300, 
+                'min' : 0, 
+            },
+            
+            'weight' : {
+                'max' : 1000, 
+                'min' : 0
+            },
+
+            'vo2Max' : {
+                'max' : 200, 
+                'min' : 0
+            },
+
+            'age' : {
+                'max' : 300, 
+                'min' : 0
+            }, 
+
+            'gender' : {
+                'max' : 1, 
+                'min' : 0
+            }, 
+
+            'caloriesEatenTarget' : {
+                'max' : 100000, 
+                'min' : 0
+            }, 
+
+            'caloriesBurnedTarget' : {
+                'max' : 100000, 
+                'min' : 0
+            }, 
+
+            'minutesTrainedTarget' : {
+                'max' : 1224, 
+                'min' : 0
+            }, 
+
+            'distanceRanTarget' : {
+                'max' : 500, 
+                'min' : 0
+            }
+        }
+
+
+
+
+'''api/food'''
+foodSchema = {
+    'type' : 'object', 
+    'properties' : {
+        'foodName' : {
+            'type' : 'string', 
+            'minLength' : 1, 
+            'maxLength' : 100
+        },
+
+        'calories' : {
+            'type' : 'number'
+        },
+
+        'fat' : {
+            'type' : 'number'
+        }, 
+
+        'carboHydrates' : {
+            'type' : 'number'
+        }, 
+
+        'protein' : {
+            'type' : 'number'
+        }
+    }
+}
+
+
+
+
+'''@loginRequired'''
+sessionValidationSchema = {
+    'type' : 'object', 
+    'properties' : {
+        'token' : {
+            'type' : 'string'
+        }, 
+        'username' : {
+            'type' : 'string', 
+            'minLength' : 1,
+            'maxLength' : 40
+        }
+    }, 
+    'required' : ['token', 'username']
 }
