94363d8d2daabdeabb70d846abed3908493d4728
Pedro Lourenco <pl156176@gmail.com>
2022-05-04 21:35:39 +0100

    fix many to many relationships

--- flask_server/models.py
+++ flask_server/models.py
@@ -1,5 +1,9 @@
 from flask_server import db
+from datetime import datetime
 
+
+
+#juction table for Workout and Exercise
 workout_exercise = db.Table('workout_exercise', 
     db.Column('workout_id', db.Integer, db.ForeignKey('workout.id')),   
     db.Column('exercise_id', db.Integer, db.ForeignKey('exercise.id'))
@@ -8,19 +12,44 @@ workout_exercise = db.Table('workout_exercise',
 
 
 
+#juction table for User and Food
+food_record = db.Table('food_record', 
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')), 
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id')), 
+    db.Column('timestamp', db.DateTime, default = datetime.utcnow)
+)
+
+
+
+
 class User(db.Model):
     __tablename__ = 'user'
 
+    #account data - one to one relationship
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True, nullable = False)
     email = db.Column(db.String, unique = True, nullable = False)
     password = db.Column(db.String, unique = False, nullable = False)
     confirmationCode = db.Column(db.String, unique = False, nullable = False)
     emailConfirmed = db.Column(db.Boolean, unique = False, default = False)
-    
-    foodRecords = db.relationship('FoodRecord', backref = 'user')
-    fitnessData = db.relationship('FitnessData', backref = 'user', uselist = False)
-    targets = db.relationship('Targets', backref = 'user', uselist = False)
+
+    #targets - one to one relationship
+    caloriesEatenTarget = db.Column(db.Integer, unique = False, nullable = True)
+    caloriesBurnedTarget = db.Column(db.Integer, unique = False, nullable = True)
+    minutesTrainedTarget = db.Column(db.Integer, unique = False, nullable = True)
+    distanceRanTarget = db.Column(db.Integer, unique = False, nullable = True)
+
+    #fitness data - one to one relationship
+    height = db.Column(db.Integer, unique = False, nullable = True)
+    weight = db.Column(db.Integer, unique = False, nullable = True)
+    vo2Max = db.Column(db.Integer, unique = False, nullable = True)
+    gender = db.Column(db.Integer, unique = False, nullable = True) #1 = male, 0 = female
+    age = db.Column(db.Integer, unique = False, nullable = True)
+
+    #FoodRecords - many-to-many relationship
+    foodRecords = db.relationship('Food', backref = 'user', secondary = food_record)
+
+    #Workouts - one-to-many relationship
     workouts = db.relationship('Workout', backref = 'user')
 
 
@@ -37,47 +66,6 @@ class Food(db.Model):
     fat = db.Column(db.Integer, unique = False, nullable = False)
     protein = db.Column(db.Integer, unique = False, nullable = False)
     carboHydrates = db.Column(db.Integer, unique = False, nullable = False)
-
-    foodRecordId = db.Column(db.Integer, db.ForeignKey('food_record.id'))
-
-
-
-
-class FoodRecord(db.Model):
-    __tablename__ = 'food_record'
-
-    id = db.Column(db.Integer, primary_key = True)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
-    foodItem = db.relationship('Food', uselist = False, backref = 'food_record')
-
-
-
-
-class FitnessData(db.Model):
-    __tablename__ = 'fitness_data'
-
-    id = db.Column(db.Integer, primary_key = True)
-    height = db.Column(db.Integer, unique = False, nullable = True)
-    weight = db.Column(db.Integer, unique = False, nullable = True)
-    vo2Max = db.Column(db.Integer, unique = False, nullable = True)
-    gender = db.Column(db.Integer, unique = False, nullable = True)
-    age = db.Column(db.Integer, unique = False, nullable = True)
-
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-
-
-class Targets(db.Model):
-    __tablename__ = 'targets'
-
-    id = db.Column(db.Integer, primary_key = True)
-    caloriesEaten = db.Column(db.Integer, unique = False, nullable = True)
-    caloriesBurned = db.Column(db.Integer, unique = False, nullable = True)
-    minutesTrained = db.Column(db.Integer, unique = False, nullable = True)
-    distanceRan = db.Column(db.Integer, unique = False, nullable = True)
-
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 
@@ -99,5 +87,6 @@ class Workout(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text, nullable = False, unique = False)
     
+    #Exercise - many-to-many relationship
     exercises = db.relationship('Exercise', secondary = workout_exercise, backref = 'exercise')
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
