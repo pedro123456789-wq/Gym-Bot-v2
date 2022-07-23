ecd260070a40a7e21cd0948d9095246c580308b4
Pedro Lourenco <pl156176@gmail.com>
2022-07-23 12:18:38 +0100

    add more validation schemes

--- flask_server/validationSchemes.py
+++ flask_server/validationSchemes.py
@@ -3,278 +3,280 @@
 
 '''/api/sign-up'''
 signUpSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'username' : {
-                        'type' : 'string', 
-                        'minLength' : 1, 
-                        'maxLength' : 20
-                    }, 
-        'password' : {
-                        'type' : 'string', 
-                        'minLength' : 7, 
-                        'maxLength' : 40, 
-                    }, 
-        'email' : {
-                    'type' : 'string', 
-                    'minLength' : 1, 
-                    'maxLength' : 40, 
-                    'pattern' : '[a-zA-Z0-9]*@[a-zA-Z0-9]*\.*[a-zA-Z0-9]*' #regex pattern for email validation
+    'type': 'object',
+    'properties': {
+        'username': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 20
+        },
+        'password': {
+            'type': 'string',
+            'minLength': 7,
+            'maxLength': 40,
+        },
+        'email': {
+            'type': 'string',
+            'minLength': 1,
+                    'maxLength': 40,
+                    # regex pattern for email validation
+                    'pattern': '[a-zA-Z0-9]*@[a-zA-Z0-9]*\.*[a-zA-Z0-9]*'
         }
-    }, 
-    'required' : ['username', 'email', 'password']
+    },
+    'required': ['username', 'email', 'password']
 }
-
-
 
 
 '''api/confirm-email'''
 confirmEmailSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'username' : {
-            'type' : 'string', 
-            'minLength' : 1, 
-            'maxLength' : 40
-        }, 
-        'confirmationCode' : {
-            'type' : 'string',
-            'length' : 5
+    'type': 'object',
+    'properties': {
+        'username': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 40
+        },
+        'confirmationCode': {
+            'type': 'string',
+            'length': 5
         }
-    }, 
-    'required' : ['username', 'confirmationCode']
+    },
+    'required': ['username', 'confirmationCode']
 }
-
-
 
 
 '''api/log-in'''
 loginSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'username' : {
-            'type' : 'string', 
-            'minLength' : 1, 
-            'maxLength' : 40
-        }, 
-        'password' : {
-            'type' : 'string', 
-            'minLength' : 1, 
-            'maxLength' : 40
+    'type': 'object',
+    'properties': {
+        'username': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 40
+        },
+        'password': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 40
         }
-    }, 
-    'required' : ['username', 'password']
+    },
+    'required': ['username', 'password']
 }
-
-
 
 
 '''api/profile'''
 profileSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'height' : {
-            'type' : 'number', 
+    'type': 'object',
+    'properties': {
+        'height': {
+            'type': 'number',
         },
 
-        'weight' : {
-            'type' : 'number'
+        'weight': {
+            'type': 'number'
         },
 
-        'vo2Max' : {
-            'type' : 'number'
+        'vo2Max': {
+            'type': 'number'
         },
 
-        'age' : {
-            'type' : 'number'
-        }, 
+        'age': {
+            'type': 'number'
+        },
 
-        'gender' : {
-            'type' : 'number'
-        }, 
+        'gender': {
+            'type': 'number'
+        },
 
-        'caloriesEatenTarget' : {
-            'type' : 'number'
-        }, 
+        'caloriesEatenTarget': {
+            'type': 'number'
+        },
 
-        'caloriesBurnedTarget' : {
-            'type' : 'number'
-        }, 
+        'caloriesBurnedTarget': {
+            'type': 'number'
+        },
 
-        'minutesTrainedTarget' : {
-            'type' : 'number'
-        }, 
+        'minutesTrainedTarget': {
+            'type': 'number'
+        },
 
-        'distanceRanTarget' : {
-            'type' : 'number'
+        'distanceRanTarget': {
+            'type': 'number'
         }
-    }, 
+    },
 
-    'required' : ['height', 'weight', 'vo2Max', 'age', 'gender', 'caloriesEatenTarget', 
-                  'caloriesBurnedTarget', 'minutesTrainedTarget', 'distanceRanTarget']
+    'required': ['height', 'weight', 'vo2Max', 'age', 'gender', 'caloriesEatenTarget',
+                 'caloriesBurnedTarget', 'minutesTrainedTarget', 'distanceRanTarget']
 }
 
 
-
-
 profileBounds = {
-            'height' : {
-                'max' : 300, 
-                'min' : 0, 
-            },
-            
-            'weight' : {
-                'max' : 1000, 
-                'min' : 0
-            },
+    'height': {
+        'max': 300,
+        'min': 0,
+    },
 
-            'vo2Max' : {
-                'max' : 200, 
-                'min' : 0
-            },
+    'weight': {
+        'max': 1000,
+        'min': 0
+    },
 
-            'age' : {
-                'max' : 300, 
-                'min' : 0
-            }, 
+    'vo2Max': {
+        'max': 200,
+        'min': 0
+    },
 
-            'gender' : {
-                'max' : 1, 
-                'min' : 0
-            }, 
+    'age': {
+        'max': 300,
+        'min': 0
+    },
 
-            'caloriesEatenTarget' : {
-                'max' : 100000, 
-                'min' : 0
-            }, 
+    'gender': {
+        'max': 1,
+        'min': 0
+    },
 
-            'caloriesBurnedTarget' : {
-                'max' : 100000, 
-                'min' : 0
-            }, 
+    'caloriesEatenTarget': {
+        'max': 100000,
+        'min': 0
+    },
 
-            'minutesTrainedTarget' : {
-                'max' : 1224, 
-                'min' : 0
-            }, 
+    'caloriesBurnedTarget': {
+        'max': 100000,
+        'min': 0
+    },
 
-            'distanceRanTarget' : {
-                'max' : 500, 
-                'min' : 0
-            }
-        }
+    'minutesTrainedTarget': {
+        'max': 1224,
+        'min': 0
+    },
 
-
+    'distanceRanTarget': {
+        'max': 500,
+        'min': 0
+    }
+}
 
 
 '''api/food'''
 foodSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'foodName' : {
-            'type' : 'string', 
-            'minLength' : 1, 
-            'maxLength' : 100
+    'type': 'object',
+    'properties': {
+        'foodName': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 100
         },
 
-        'calories' : {
-            'type' : 'number'
+        'calories': {
+            'type': 'number'
         },
 
-        'fat' : {
-            'type' : 'number'
-        }, 
+        'fat': {
+            'type': 'number'
+        },
 
-        'carboHydrates' : {
-            'type' : 'number'
-        }, 
+        'carboHydrates': {
+            'type': 'number'
+        },
 
-        'protein' : {
-            'type' : 'number'
+        'protein': {
+            'type': 'number'
         }
-    }, 
+    },
 
-    'required' : ['foodName', 'calories', 'fat', 'carboHydrates', 'protein']
+    'required': ['foodName', 'calories', 'fat', 'carboHydrates', 'protein']
 }
-
-
-
 
 
 '''api/body-fat-prediction'''
 bodyFatPredictionSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'weight' : {
-            'type' : 'number'
-        }, 
-
-        'chest' : {
-            'type' : 'number'
-        }, 
-
-        'abdomen' : {
-            'type' : 'number'
+    'type': 'object',
+    'properties': {
+        'weight': {
+            'type': 'number'
         },
 
-        'hip' : {
-            'type' : 'number'
+        'chest': {
+            'type': 'number'
+        },
+
+        'abdomen': {
+            'type': 'number'
+        },
+
+        'hip': {
+            'type': 'number'
         }
-    }, 
-    'required' : ['weight', 'chest', 'abdomen', 'hip'] 
-} 
-
-
+    },
+    'required': ['weight', 'chest', 'abdomen', 'hip']
+}
 
 
 '''api/workouts  => POST'''
 workoutsSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'name' : {
-            'type' : 'string', 
-            'minLength' : 1, 
-            'maxLength' : 30
+    'type': 'object',
+    'properties': {
+        'name': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 30
+        },
+        'caloriesBurned': {
+            'type': 'number'
         }
-    }, 
-    'required' : ['name']
+    },
+    'required': ['name']
 }
 
 
-
-
 exerciseSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'name' : {
-            'type' : 'string',
-            'minLength' : 1, 
-            'maxLength' : 30
-        }, 
-        'durationSeconds' : {
-            'type' : 'number'
-        }, 
-        'repetitions' : {
-            'type' : 'number'
+    'type': 'object',
+    'properties': {
+        'name': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 30
+        },
+        'durationSeconds': {
+            'type': 'number'
+        },
+        'repetitions': {
+            'type': 'number'
         }
     }
 }
 
 
+'''api/runs => POST'''
+runSchema = {
+    'type': 'object',
+    'properties': {
+        'distance': {
+            'type': 'number',
+        },
+        'durationSeconds': {
+            'type': 'number'
+        },
+        'caloriesBurned': {
+            'type': 'number'
+        }
+    }
+}
 
 
 '''@loginRequired'''
 sessionValidationSchema = {
-    'type' : 'object', 
-    'properties' : {
-        'token' : {
-            'type' : 'string'
-        }, 
-        'username' : {
-            'type' : 'string', 
-            'minLength' : 1,
-            'maxLength' : 40
+    'type': 'object',
+    'properties': {
+        'token': {
+            'type': 'string'
+        },
+        'username': {
+            'type': 'string',
+            'minLength': 1,
+            'maxLength': 40
         }
-    }, 
-    'required' : ['token', 'username']
+    },
+    'required': ['token', 'username']
 }
